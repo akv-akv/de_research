88251d955df12a9867583fdd7b9aab084a84557a
Kirill Avilenko <drzoob@gmail.com>
2024-08-14 23:30:23 +0400

    chore: add pre-commit hooks for terraform

--- cloud_infrastructure/terraform/aws/network/provider.tf
+++ cloud_infrastructure/terraform/aws/network/provider.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.62.0"
+    }
+  }
+}
+
 provider "aws" {
-    shared_config_files      = ["~/.aws/config"]
-    profile                  = "akv"
+  shared_config_files = ["~/.aws/config"]
+  profile             = "akv"
 }
